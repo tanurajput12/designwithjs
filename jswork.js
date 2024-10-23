77c65f11ebb506824b7a0561dbffa2fbb460cf59
tanurajput12 <tanurajput1508@gmail.com>
2024-10-23 20:51:04 +0530

    Add files via upload

---
+++ jswork.js
@@ -0,0 +1,26 @@
+function show(arg){
+    if(arg===1){
+        let store=document.querySelector('#output');
+        store.style.backgroundImage='url("https://m.media-amazon.com/images/I/91s7rC+IDBL._SX679_.jpg ")'; 
+    }
+     else if(arg===2){
+        let store=document.querySelector('#output');
+        store.style.backgroundImage= 'url("https://m.media-amazon.com/images/I/817p7j0CaFL._AC_SY200_.jpghttps://m.media-amazon.com/images/I/91s7rC+IDBL._SX679_.jpg ")';
+        
+    }
+    if(arg===3){
+        let store=document.querySelector('#output');
+        store.style.backgroundImage='url("https://m.media-amazon.com/images/I/81IoTi3WNiL._AC_SY200_.jpghttps://m.media-amazon.com/images/I/91s7rC+IDBL._SX679_.jpg ")';
+        
+    }
+    if(arg===4){
+        let store=document.querySelector('#output');
+        store.style.backgroundImage='url("https://m.media-amazon.com/images/I/41QYY6zfX3L._SY445_SX342_QL70_FMwebp_.jpghttps://m.media-amazon.com/images/I/91s7rC+IDBL._SX679_.jpg ")'; 
+        
+    }
+    if(arg===5){
+        let store=document.querySelector('#output');
+        store.style.backgroundImage='url("https://m.media-amazon.com/images/S/al-eu-726f4d26-7fdb/3c452182-f887-4812-bc9c-91f144ebf16a._CR0,1562,3563,1865_SX507_QL70_.jpghttps://m.media-amazon.com/images/I/91s7rC+IDBL._SX679_.jpg ")'; 
+        
+    }
+}
